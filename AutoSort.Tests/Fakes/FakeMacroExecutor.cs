bf40f08a295f92e1c30800de40f2e7c26844c71e
Valiice <valentinclaes@live.be>
2026-03-29 18:52:35 +0200

    Materialise command list in FakeMacroExecutor to prevent snapshot trap

--- AutoSort.Tests/Fakes/FakeMacroExecutor.cs
+++ AutoSort.Tests/Fakes/FakeMacroExecutor.cs
@@ -4,6 +4,6 @@ namespace AutoSort.Tests.Fakes;
 
 public class FakeMacroExecutor : IMacroExecutor
 {
-    public List<IEnumerable<string>> Calls { get; } = new();
-    public void Execute(IEnumerable<string> commands) => Calls.Add(commands);
+    public List<List<string>> Calls { get; } = new();
+    public void Execute(IEnumerable<string> commands) => Calls.Add(commands.ToList());
 }
